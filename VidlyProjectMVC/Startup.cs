7e125b4ce422c186c9c467591db564d7aab88262
Wingrao <56922415+Wingrao@users.noreply.github.com>
2019-12-16 12:04:27 +0200

    Add project files.

---
+++ VidlyProjectMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyProjectMVC.Startup))]
+namespace VidlyProjectMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
